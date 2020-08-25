cb2cab90392442c90bc2b3edae6366329d3b9b55
Niko Para <niko@niko.io>
2020-08-25 14:08:49 +0200

    some important changes

--- config.el
+++ config.el
@@ -22,7 +22,9 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-  (setq doom-font (font-spec :family "Iosevka" :size 14 :weight 'light))
+
+(setq doom-font (font-spec :family "Iosevka Term" :size 13 :weight 'light))
+;; (setq doom-font (font-spec :family "Source Code Pro" :size 16))
  ;; (setq doom-font (font-spec :family "Noto Serif" :size 14 :weight 'regular))
  (setq doom-variable-pitch-font (font-spec :family "Liberation Serif" :size 14))
 
@@ -72,6 +74,8 @@
 	)
 (add-hook 'web-mode-hook #'indent-force)
 (add-hook 'typescript-mode-hook #'indent-force)
+(add-hook 'js2-mode-hook #'indent-force)
+(add-hook 'rjsx-mode-hook #'indent-force)
 
 (setq tide-tsserver-executable "node_modules/typescript/bin/tsserver")
 
@@ -115,24 +119,30 @@
                                  ("===" . ?≡)
                                  ("<=" . ?≤)
 				 ("if" . ??)
-				 ("const" . ?●)
-				 ("import" . ?●)
-				 ("from" . ?●)
+				 ;; ("const" . ?●) dot
+				 ;; ("import" . ?●)
+				 ;; ("from" . ?●)
+				 ;; ("const " . ?●)
+				 ("import" . ?_)
+				 ("from " . ?_)
 				 ;; ("const" . ?○)
 				 ;;  ("import" . ?○)
 				 ;; ("from" . ?○)
 				 ("number" . ?Z)
 				 ("string" . ?Σ)
 				 ("boolean" . ?B)
+				 ("function" . ?Ƒ)
 				 ;; ("import" . ?⭆) ;; tripple arrow import
-				 ("export" . ?⭅)
-				 ("return" . ?⮩)
-				 ("Promise" . ?⧖)
-				 ("async" . ?⧖)
-				 ("await" . ?⧖)
+				 ("export" . ?←)
+				 ;; ("return" . ?⮩)
+				 ("return" . ?↳)
+				 ("Promise" . ?∞)
+				 ("async" . ?∞)
+				 ("await" . ?∞)
 				 ("curry" . ?∂)
 				 ("void" . ?∅)
 				 ("partial" . ?∂)
+				 ("Partial" . ?∂)
                                  (">=" . ?≥)
                                  ;;("=<<" . ?=≪)
                                  ;;(">>=" . ?≫=)
@@ -198,22 +208,22 @@
 
 (require 'company-org-roam)
 (push 'company-org-roam company-backends)
-(add-hook 'after-change-major-mode-hook 'change-my-background-color)
 
-(add-hook 'window-configuration-change-hook 'change-my-background-color)
+;; (add-hook 'after-change-major-mode-hook 'change-my-background-color)
+;; (add-hook 'window-configuration-change-hook 'change-my-background-color)
 
-(setq xxx 0)
-(defun change-my-background-color ()
-  (cond
-    ((eq major-mode 'org-mode)
-      (eq xxx 0)
-      (setq xxx 1)
-      (load-theme 'mono-light t))
-    ((eq major-mode 'python-mode)
-      (eq xxx 1)
-      (setq xxx 0)
-      (load-theme 'mono-dark t))))
-
+;; (setq xxx 0)
+;; (defun change-my-background-color ()
+;;   (cond
+;;     ((eq major-mode 'org-mode)
+;;       (eq xxx 0)
+;;       (setq xxx 1)
+;;       (load-theme 'mono-light t))
+;;     ((eq major-mode 'python-mode)
+;;       (eq xxx 1)
+;;       (setq xxx 0)
+;;       (load-theme 'mono-dark t))))
+;; 
 (defun lights-on () (interactive) (load-theme 'mono-light t))
 (defun lights-off () (interactive) (load-theme 'mono-dark t))
 
